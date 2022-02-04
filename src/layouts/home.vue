0c7f169bb4242e395d14f54bc72cf2c7d06befe5
liyasthomas <liyascthomas@gmail.com>
2022-02-04 10:23:37 +0530

    chore: updates

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,6 +1,6 @@
 <template>
   <main
-    class="container flex flex-col border-l border-r border-dashed divide-y border-dividerLight divide-dividerLight"
+    class="container flex flex-col transition border-l border-r border-dashed divide-y border-dividerLight divide-dividerLight"
   >
     <HomeHeader />
     <router-view />
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col divide-y divide-dividerLight">
+  <div class="flex flex-col transition divide-y divide-dividerLight">
     <LandingHero />
     <LandingUsers />
     <LandingFeatures />
