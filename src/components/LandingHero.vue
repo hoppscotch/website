cd128bfc137f4ce721985812df544fe7702ccd1f
Liyas Thomas <liyascthomas@gmail.com>
2022-12-16 22:09:49 +0530

    chore: minor ui update

--- src/components/LandingHero.vue
+++ src/components/LandingHero.vue
@@ -20,14 +20,14 @@ onMounted(() => {
 <template>
   <div class="flex flex-col px-8 py-16">
     <LandingAnnouncement />
-    <div class="flex flex-col items-center">
+    <div class="sticky flex flex-col items-center top-32">
       <h1
-        class="block w-full max-w-2xl mt-4 text-5xl leading-none tracking-tighter text-center transition text-secondaryDark interactive-font md:text-6xl lg:text-7xl"
+        class="block w-full max-w-2xl mt-4 text-5xl leading-none tracking-tighter text-center transition text-secondaryDark interactive-font md:text-6xl lg:text-8xl"
       >
         {{ t("home.hero.heading") }}
       </h1>
       <h1
-        class="block w-full max-w-2xl mb-4 text-5xl leading-none tracking-tighter text-center text-transparent transition bg-clip-text bg-gradient-to-r interactive-font md:text-6xl lg:text-7xl"
+        class="block w-full max-w-2xl mb-4 text-5xl leading-none tracking-tighter text-center text-transparent transition bg-clip-text bg-gradient-to-r interactive-font md:text-6xl lg:text-8xl"
         :class="gradients.accent"
       >
         {{ t("home.hero.heading_line_2") }}
@@ -35,7 +35,7 @@ onMounted(() => {
       <p class="max-w-md my-4 text-xl tracking-tighter text-center md:w-3/5">
         {{ t("home.hero.subheading") }}
       </p>
-      <p class="flex my-4 text-center space-x-2">
+      <p class="flex my-4 space-x-2 text-center">
         <ButtonSecondary
           to="https://github.com/hoppscotch/hoppscotch"
           label="GitHub"
@@ -52,21 +52,21 @@ onMounted(() => {
         {{ t("home.hero.stats.2.title") }} &nbsp; • &nbsp;
         {{ t("home.hero.stats.3.title") }}
       </p>
-      <div
-        class="relative flex p-1 mt-8 rounded bg-gradient-to-r md:rounded-xl"
-        :class="gradients.accent"
-      >
-        <SmartGradient animate :blur="256" />
-        <SmartGradient animate :blur="64" />
-        <img
-          class="object-cover object-center w-full h-full rounded shadow-2xl transition md:rounded-xl"
-          :src="`/assets/images/screenshots/${
-            isDark ? 'dark' : 'light'
-          }-banner.png`"
-          alt="Screenshot"
-          loading="lazy"
-        />
-      </div>
+    </div>
+    <div
+      class="relative flex p-1 mt-8 rounded bg-gradient-to-r md:rounded-xl"
+      :class="gradients.accent"
+    >
+      <SmartGradient animate :blur="256" />
+      <SmartGradient animate :blur="64" />
+      <img
+        class="object-cover object-center w-full h-full transition rounded shadow-2xl md:rounded-xl"
+        :src="`/assets/images/screenshots/${
+          isDark ? 'dark' : 'light'
+        }-banner.png`"
+        alt="Screenshot"
+        loading="lazy"
+      />
     </div>
   </div>
 </template>
