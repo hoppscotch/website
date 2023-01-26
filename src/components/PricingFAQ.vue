88c9e0e46f2fe0e1e0a000aa9c0ff2e7943cd0d1
Liyas Thomas <liyascthomas@gmail.com>
2023-01-26 23:27:32 +0530

    chore: updated ui

--- src/components/PricingFAQ.vue
+++ src/components/PricingFAQ.vue
@@ -3,30 +3,26 @@ import { faq } from "~/assets/data/pricing"
 </script>
 
 <template>
-  <div class="flex flex-col px-8 py-16">
+  <div class="flex items-center justify-center px-8 py-16">
     <div
       class="flex flex-col flex-1 max-w-2xl border rounded-lg divide-y divide-divider bg-primaryLight border-divider"
     >
-      <details v-for="(item, index) in faq" :key="index" class="group">
+      <details
+        v-for="(item, index) in faq"
+        :key="index"
+        class="group"
+        :open="item.open"
+      >
         <summary
-          class="flex items-center justify-between font-medium list-none cursor-pointer"
+          class="flex items-center justify-between font-medium list-none cursor-pointer focus:outline-none"
         >
-          <span class="flex flex-1 px-8 py-4">{{ item.question }}</span>
-          <span class="transition">
-            <svg
-              fill="none"
-              height="24"
-              shape-rendering="geometricPrecision"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              viewBox="0 0 24 24"
-              width="24"
-            >
-              <path d="M6 9l6 6 6-6"></path>
-            </svg>
-          </span>
+          <div class="flex items-center justify-between flex-1 px-8 py-4">
+            {{ item.question }} {{ item.open }}
+            <lucide-chevron-up
+              class="transition transform"
+              :class="{ 'rotate-180': item.open }"
+            />
+          </div>
         </summary>
         <div
           class="px-8 py-4 border-t bg-primary border-divider text-neutral-600"
