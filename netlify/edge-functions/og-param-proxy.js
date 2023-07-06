abe376b81c78199ac74c7664aaabdca5e17416a0
Liyas Thomas <liyascthomas@gmail.com>
2023-07-06 16:05:57 +0530

    fix: netlify edge function

--- netlify/edge-functions/og-param-proxy.js
+++ netlify/edge-functions/og-param-proxy.js
@@ -1,14 +1,13 @@
 export default async (request, context) => {
-  const url = new URL(request.url)
-
   // Get the page content.
   const response = await context.next()
   const page = await response.text()
-
   // Look for the OG image generator path.
   const search = "https://next.hoppscotch.com/.netlify/functions/generator"
   // Replace it with the path plus the querystring.
-  const replace = `https://next.hoppscotch.com/.netlify/functions/generator?${url.searchParams.toString()}`
+  const replace = `https://next.hoppscotch.com/.netlify/functions/generator?key=${
+    request.url.split("/blog/")[1]
+  }`
 
   return new Response(page.replaceAll(search, replace), response)
 }
