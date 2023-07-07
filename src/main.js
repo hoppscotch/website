a88a457d74e8dca30b585b84a3bd4090fc6567bc
Liyas Thomas <liyascthomas@gmail.com>
2023-07-07 08:41:02 +0530

    refactor: og changelog

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { createApp } from "vue"
 import { createHead } from "@unhead/vue"
-import router from "./router"
 import App from "./App.vue"
 import VueTippy, { roundArrow } from "vue-tippy"
 import "tippy.js/dist/tippy.css"
@@ -8,30 +6,51 @@ import "tippy.js/dist/svg-arrow.css"
 import "tippy.js/themes/light.css"
 import "tippy.js/animations/scale-subtle.css"
 import "aos/dist/aos.css"
-import "nprogress/nprogress.css"
 import "./styles/style.scss"
-const app = createApp(App)
 const head = createHead()
-app.use(head)
-app.use(router)
-app.use(VueTippy, {
-  defaultProps: {
-    animation: "scale-subtle",
-    allowHTML: false,
-    animateFill: false,
-    arrow: roundArrow + roundArrow,
-    theme: "light",
-    offset: [0, 16],
-    popperOptions: {
-      modifiers: [
-        {
-          name: "preventOverflow",
-          options: {
-            rootBoundary: "document",
-          },
-        },
-      ],
+import { setupLayouts } from "virtual:generated-layouts"
+import generatedRoutes from "virtual:generated-pages"
+const routes = setupLayouts(generatedRoutes)
+import { ViteSSG } from "vite-ssg"
+export const createApp = ViteSSG(
+  App,
+  {
+    routes,
+    base: import.meta.env.BASE_URL,
+    scrollBehavior(to, from, savedPosition) {
+      if (to.hash) {
+        return {
+          el: to.hash,
+          behavior: "smooth",
+          top: 32,
+        }
+      } else {
+        return { top: 0, behavior: "smooth" }
+      }
     },
   },
-})
-app.mount("#app")
+  ({ app, router, routes, isClient, initialState }) => {
+    app.use(head)
+    app.use(router)
+    app.use(VueTippy, {
+      defaultProps: {
+        animation: "scale-subtle",
+        allowHTML: false,
+        animateFill: false,
+        arrow: roundArrow + roundArrow,
+        theme: "light",
+        offset: [0, 16],
+        popperOptions: {
+          modifiers: [
+            {
+              name: "preventOverflow",
+              options: {
+                rootBoundary: "document",
+              },
+            },
+          ],
+        },
+      },
+    })
+  }
+)
