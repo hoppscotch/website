4c53a7e7c1a4c4d0d4f3966fc1810d1f6427a5ef
Liyas Thomas <liyascthomas@gmail.com>
2023-10-28 09:25:28 +0530

    chore: updates

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="relative flex mb-16 z-1">
     <RouterLink
-      to="/blog/introducing-hoppscotch-desktop-application"
+      to="/blog/introducing-hoppscotch-enterprise-edition"
       class="relative inline-flex items-center justify-center px-4 py-2 text-sm border rounded-full transition backdrop-blur-md bg-gradient-to-b from-neutral-950 via-neutral-950 border-neutral-500/50 hover:to-white/5 hover:border-neutral-500/75"
     >
       <span
         class="inline-flex text-transparent bg-clip-text max-w-max bg-gradient-to-b from-neutral-50 to-neutral-600"
       >
-        <span class="mr-2">✦</span> Introducing Hoppscotch Desktop Application
+        <span class="mr-2">✦</span> Introducing Hoppscotch Self-Host Enterprise
       </span>
     </RouterLink>
   </div>
