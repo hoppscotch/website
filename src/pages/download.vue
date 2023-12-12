22c274ee14789d61fe6d34c0c89142350e94fc4d
Liyas Thomas <liyascthomas@gmail.com>
2023-12-12 19:02:02 +0530

    updates

--- src/pages/download.vue
+++ src/pages/download.vue
@@ -3,8 +3,40 @@
     title: "Download • Hoppscotch",
     meta: [
       {
+        property: "og:title",
+        content: "Download Hoppscotch for your platform.",
+      },
+      {
+        name: "twitter:title",
+        content: "Download • Hoppscotch",
+      },
+      {
         name: "description",
         content: "Download Hoppscotch for your platform.",
+      },
+      {
+        property: "og:description",
+        content: "Download Hoppscotch for your platform.",
+      },
+      {
+        name: "twitter:description",
+        content: "Download Hoppscotch for your platform.",
+      },
+      {
+        property: "og:image",
+        content: "https://hoppscotch.com/banner-download.png",
+      },
+      {
+        name: "twitter:image",
+        content: "https://hoppscotch.com/banner-download.png",
+      },
+      {
+        property: "og:type",
+        content: "article",
+      },
+      {
+        property: "og:url",
+        content: "https://hoppscotch.com/download",
       },
     ],
   })
