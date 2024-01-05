98dc50986d55e00ad8351383da24f98f3b3fb376
Liyas Thomas <liyascthomas@gmail.com>
2024-01-05 15:11:09 +0530

    updates

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -40,11 +40,11 @@ import type {
 declare module "vue-router/auto/routes" {
   export interface RouteNamedMap {
     "/": RouteRecordInfo<"/", "/", Record<never, never>, Record<never, never>>
-    "/[... path]": RouteRecordInfo<
-      "/[... path]",
-      "/: path(.*)",
-      { path: ParamValue<true> },
-      { path: ParamValue<false> }
+    "/[...all]": RouteRecordInfo<
+      "/[...all]",
+      "/:all(.*)",
+      { all: ParamValue<true> },
+      { all: ParamValue<false> }
     >
     "/about": RouteRecordInfo<
       "/about",
