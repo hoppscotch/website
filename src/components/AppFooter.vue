7f2fbf024e2be975f4b718e57bfec61dad6b51a6
Liyas Thomas <liyascthomas@gmail.com>
2024-01-11 13:58:08 +0530

    chore: update dependencies and fix styling in AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,7 +9,7 @@
     <div class="relative max-w-5xl px-4 mx-auto sm:px-6">
       <!-- Footer links -->
       <!-- <FooterLinks /> -->
-      <div class="py-4 grid gap-8 sm:grid-cols-12 md:py-8">
+      <div class="grid gap-8 py-4 sm:grid-cols-12 md:py-8">
         <!-- Block -->
         <div class="col-span-12">
           <div
@@ -19,7 +19,7 @@
               <div class="mb-4">
                 <!-- Logo -->
                 <RouterLink
-                  class="inline-flex items-center justify-center p-2 border rounded-full outline-none transition border-zinc-500/50 hover:border-zinc-500/80 hover:scale-110"
+                  class="inline-flex items-center justify-center p-2 transition border rounded-full outline-none border-zinc-500/50 hover:border-zinc-500/80 hover:scale-110"
                   to="/"
                   aria-label="Hoppscotch"
                 >
@@ -32,7 +32,8 @@
                 </RouterLink>
               </div>
               <div class="text-xs text-zinc-400/80">
-                &copy; 2023 Hoppscotch Limited. All rights reserved.
+                &copy; {{ new Date().getFullYear() }} Hoppscotch Limited. All
+                rights reserved.
               </div>
             </div>
             <!-- Social links -->
@@ -43,7 +44,7 @@
                     href="https://status.hoppscotch.io"
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="flex items-center text-xs rounded transition text-zinc-400/80 hover:text-zinc-100"
+                    class="flex items-center text-xs transition rounded text-zinc-400/80 hover:text-zinc-100"
                   >
                     <div class="w-1 h-1 mr-2 bg-green-500 rounded-full"></div>
                     All systems operational.
@@ -55,7 +56,7 @@
               >
                 <li class="border-r border-zinc-800">
                   <button
-                    class="flex items-center justify-center rounded-full transition text-zinc-400/80 hover:text-zinc-100"
+                    class="flex items-center justify-center transition rounded-full text-zinc-400/80 hover:text-zinc-100"
                     @click="toggleDark()"
                   >
                     <icon-lucide-moon v-if="isDark" class="w-8 h-8 p-2" />
@@ -64,7 +65,7 @@
                 </li>
                 <li>
                   <a
-                    class="flex items-center justify-center rounded-full transition text-zinc-400/80 hover:text-zinc-100"
+                    class="flex items-center justify-center transition rounded-full text-zinc-400/80 hover:text-zinc-100"
                     href="https://twitter.com/hoppscotch_io"
                     aria-label="Twitter"
                     target="_blank"
@@ -75,7 +76,7 @@
                 </li>
                 <li>
                   <a
-                    class="flex items-center justify-center rounded-full transition text-zinc-400/80 hover:text-zinc-100"
+                    class="flex items-center justify-center transition rounded-full text-zinc-400/80 hover:text-zinc-100"
                     href="https://www.linkedin.com/company/hoppscotch"
                     aria-label="Discord"
                     target="_blank"
@@ -86,7 +87,7 @@
                 </li>
                 <li>
                   <a
-                    class="flex items-center justify-center rounded-full transition text-zinc-400/80 hover:text-zinc-100"
+                    class="flex items-center justify-center transition rounded-full text-zinc-400/80 hover:text-zinc-100"
                     href="https://github.com/hoppscotch/hoppscotch"
                     aria-label="Github"
                     target="_blank"
