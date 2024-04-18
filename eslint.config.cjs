49c41b427429f6ac711ccc1b183ee38341b83bfc
Liyas Thomas <liyascthomas@gmail.com>
2024-04-18 15:36:33 +0530

    chore: updated deps

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,19 +1,20 @@
-{
-  "root": true,
-  "extends": [
+module.exports = {
+  root: true,
+  extends: [
     "eslint:recommended",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
     "plugin:vue/vue3-recommended",
-    "prettier"
+    "prettier",
   ],
-  "rules": {
+  rules: {
     "vue/multi-word-component-names": "off",
     "vue/max-attributes-per-line": "off",
     "vue/html-self-closing": "off",
-    "vue/singleline-html-element-content-newline": "off"
+    "vue/singleline-html-element-content-newline": "off",
   },
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  }
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  ignorePatterns: ["node_modules/", "dist/", "public/"],
 }
