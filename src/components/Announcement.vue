e23ed2542671496e60ef68af945f94b07d14de81
Liyas Thomas <liyascthomas@gmail.com>
2025-11-30 23:19:38 -0600

    Update announcement link and message to reflect the launch of API Mocking

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="z-1 relative mb-16 flex">
     <RouterLink
-      to="/blog/introducing-hoppscotch-desktop-application"
+      to="/blog/hoppscotch-v2025-10-0"
       class="relative inline-flex items-center justify-center rounded-full border border-zinc-800 bg-gradient-to-b from-zinc-950 via-zinc-950 px-4 py-2 text-sm shadow-md backdrop-blur-md transition hover:border-zinc-700 hover:to-white/5"
     >
       <span
         class="inline-flex max-w-max items-center justify-center bg-gradient-to-br from-zinc-50 to-zinc-600 bg-clip-text text-transparent"
       >
-        <span class="mr-2">✦</span> Introducing Hoppscotch Desktop Application
+        <span class="mr-2">✦</span> Now live: API Mocking
       </span>
     </RouterLink>
   </div>
